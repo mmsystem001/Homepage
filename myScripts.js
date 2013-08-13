5487707fea80cd67aa4f58938a620051610fa200
mmsystem001 <mmsystem001@yahoo.com>
2013-08-13 12:14:08 +0800

    jquery

---
+++ myScripts.js
@@ -0,0 +1,11 @@
+$('#fadeout').click(function(){
+	$('#text').fadeOut('slow');
+});
+
+$('#fadein').click(function(){
+	$('#text').fadeIn('slow');
+});
+
+$('#dropdown').click(function(){
+	$('#text').fadeIn('slow');
+});
